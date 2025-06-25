db002d3d30fb5a793d6d3377900cc2e43b68e54f
Daniel Fe <daniel.fe.92@gmail.com>
2025-06-25 01:56:49 -0300

    added linting changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
